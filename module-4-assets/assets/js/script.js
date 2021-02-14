564843f59dad98dea0d19ab46a861f9395c22da1
Your Name <richardbriansantos@Richards-MacBook-Pro.local>
2021-02-14 12:25:08 -0800

    added addEventListener

--- module-4-assets/assets/js/script.js
+++ module-4-assets/assets/js/script.js
@@ -1,2 +1,10 @@
 var buttonEl = document.querySelector("#save-task");
 console.log(buttonEl);
+var taskToDoEl = document.querySelector("#task-to-do");
+
+buttonEl.addEventListener("click", function() {
+    var listItemEl = document.createElement("li");
+    listItemEl.className = "task-item";
+    listItemEl.textContent = "This is a new task.";
+    taskToDoEl.appendChild(listItemEl);
+});
